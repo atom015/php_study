7cea1820b4e28454cea5507d423f5c79e3024225
김승우 <zeezlelove@gmail.com>
2020-04-09 13:42:24 +0900

    php 공부하기

---
+++ value.php
@@ -0,0 +1,14 @@
+<?php 
+	// 변수선언 밑 출력
+	$a = 3;
+	echo $a.' ';
+	$b = "Hello";
+	echo $b." php ";
+	// 변수타입 체크
+	echo gettype($a).' '; 
+	echo gettype($b).' ';
+	//가변변수
+	$d = 'tmp';
+	$$d = 'Hello';
+	echo $tmp
+?>
